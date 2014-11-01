8c843b97a6a06c7f9bf14ff3145dea27494bc22e
Amin Sojoudi <masojoudi@gmail.com>
2014-11-01 23:46:12 +0330

    * Added ToastHandler

---
+++ Assets/Scripts/ToastHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToastHandler : MonoBehaviour {
+
+	public static ToastHandler Instance;
+
+	void Awake()
+	{
+		Instance = this;
+	}
+
+
+	public void Toast(string message , ToastType type)
+	{
+
+	}
+
+}
+
+public enum ToastType {
+	Long ,
+	Short
+}
